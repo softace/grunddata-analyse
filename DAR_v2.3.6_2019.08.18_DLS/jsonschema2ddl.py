33fe9455a29e0160100162d40b756e0895e2229f
Jarl Friis <jarl@softace.dk>
2020-03-05 19:02:57 +0100

    Initial script from JSON skema

---
+++ DAR_v2.3.6_2019.08.18_DLS/jsonschema2ddl.py
@@ -0,0 +1,20 @@
+import os
+import json
+from pprint import pprint
+
+# read JSON into data structure
+with open("DAR_v2.3.6_2019.08.19_DARTotal.schema.json") as file:
+    jsonschema = json.load(file)
+
+# print DDL
+#pprint(jsonschema)
+
+for (table_name, table_content) in jsonschema['properties'].items():
+    assert(table_content['type'] == 'array')
+    print("CREATE TABLE " + table_name + "(")
+    for (att_name, att_content) in table_content['items']['properties'].items():
+        print(f"  {att_name: <20} {att_content['type'][0]: <10} {att_content['type'][1]},")
+#        pprint(att_content['type'])
+    print(");")
+
+#json.dump(jsonschema)
