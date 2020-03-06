fa8f53c60e05a3ae9d5cc70b51d4ba0c75287284
Jarl Friis <jarl@softace.dk>
2020-03-06 06:53:39 +0100

    moved files

--- jsonschema2ddl.py
+++ jsonschema2ddl.py
@@ -13,8 +13,10 @@ for (table_name, table_content) in jsonschema['properties'].items():
     assert(table_content['type'] == 'array')
     print("CREATE TABLE " + table_name + "(")
     for (att_name, att_content) in table_content['items']['properties'].items():
-        print(f"  {att_name: <20} {att_content['type'][0]: <10} {att_content['type'][1]},")
+#        print(f"  {att_name: <20} {att_content['type'][0]: <10} {att_content['type'][1]},")
+        print(f"  {att_name: <20} {'TEXT': <10},")
 #        pprint(att_content['type'])
+    print("PRIMARY KEY(id_lokalId (ASC)")
     print(");")
 
 #json.dump(jsonschema)
