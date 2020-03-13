c43c5c1f6719555fc738d9709940caff1e18ebb4
Jarl Friis <jarl@softace.dk>
2020-03-13 19:01:26 +0100

    Performance improvements

--- load_dar.py
+++ load_dar.py
@@ -8,68 +8,77 @@ import sqlite3
 import contextlib
 import time
 #from zipstream import ZipFile
-step_rows = 1000000
+STEP_ROWS = 10000
 
+table_names = {}
 
-
-def insert_row(connection, listName, row):
-    columns = row.keys()
-    values = ['NULL' if (x is None) else x for x in row.values()]
+def insert_row(cursor, listName, row):
+    values = [x if x else 'NULL' for x in row.values()]
     if row['id_lokalId'] is None or row['registreringFra'] is None or row['virkningFra'] is None:
         raise ValueError(f"Forventet primær nøgle (id_lokalId, registreringFra, virkningFra) har egentlig værdier, men fandt ({row['id_lokalId']}, {row['registreringFra']}, {row['virkningFra']})")
     if row['virkningTil']: #This is an update
-        cursor = connection.cursor()
-        original = "select id_lokalId, registreringFra, virkningFra " \
-                   f"from {listName} where true " \
-                   "AND id_lokalId = ? " \
-                   "AND registreringFra = ? " \
-                   "AND virkningFra = ?"
-        cursor.execute(original, [row['id_lokalId'], row['registreringFra'], row['virkningFra']])
+        cursor.execute(table_names[listName]['F'], [row['id_lokalId'], row['registreringFra'], row['virkningFra']])
         rows = cursor.fetchall()
         if len(rows) > 1:
             raise ValueError(f"Forventet at opdatere een forekomst for ({row['id_lokalId']}, {row['registreringFra']}, {row['virkningFra']}), fandt {len(rows)} forekomster")
         else:
-            update = f"update {listName} set " + \
-                     ", ".join([f" {c} = ? " for c in columns]) + " where true "\
-                     "AND id_lokalId = ? " \
-                     "AND registreringFra = ? " \
-                     "AND virkningFra = ?"
-            cursor = connection.cursor()
-            cursor.execute(update, values + [row['id_lokalId'], row['registreringFra'], row['virkningFra']])
+            cursor.execute(table_names[listName]['U'], values + [row['id_lokalId'], row['registreringFra'], row['virkningFra']])
             return 1
     else:
-        select = f"select * from {listName} where true " \
-                 "AND registreringFra >= ? " \
-                 "AND (? < registreringTil or registreringTil is NULL) " \
-                 "AND virkningFra >= ? " \
-                 "AND ? < virkningTil or virkningTil is NULL"
-        cursor = connection.cursor()
-        cursor.execute(select, [row['registreringFra'], row['registreringTil'], row['virkningFra'], row['virkningTil']])
+        cursor.execute(table_names[listName]['V'], [row['registreringFra'], row['registreringTil'], row['virkningFra'], row['virkningTil']])
         violations = cursor.fetchall()
         if len(violations) > 0:
             raise ValueError(f"Der findes allerede en forekomst i registreringstid ({row['registreringFra']}; {row['registreringTil']}) og virkningstid({row['virkningFra']}, {row['virkningTil']})")
-        SQL = f" INSERT into {listName} ({', '.join(columns)}) VALUES({', '.join(['?' for x in range(len(values))])});"
-        connection.execute(SQL, values)
+        cursor.execute(table_names[listName]['I'], values)
         return 0
 
 
-def initialise_dar(db_name):
+def prepare_table(table_name, columns):
+    table_names[table_name] = {}
+    table_names[table_name]['F'] = "select id_lokalId, registreringFra, virkningFra " \
+                                   f"from {table_name} where true " \
+                                   "AND id_lokalId = ? " \
+                                   "AND registreringFra = ? " \
+                                   "AND virkningFra = ?"
+    table_names[table_name]['U'] = f"update {table_name} set " + \
+                                   ", ".join([f" {c} = ? " for c in columns]) + " where true "\
+                                   "AND id_lokalId = ? " \
+                                   "AND registreringFra = ? " \
+                                   "AND virkningFra = ?"
+    table_names[table_name]['V'] = f"select * from {table_name} where true " \
+                                   "AND registreringFra >= ? " \
+                                   "AND (? < registreringTil or registreringTil is NULL) " \
+                                   "AND virkningFra >= ? " \
+                                   "AND ? < virkningTil or virkningTil is NULL"
+    table_names[table_name]['I'] = f" INSERT into {table_name} ({', '.join(columns)})"\
+                                   f" VALUES({', '.join(['?' for x in range(len(columns))])});"
+
+
+def initialise_dar(db_name, create=False):
     with open("DAR_v2.3.6_2019.08.18_DLS/DAR_v2.3.6_2019.08.19_DARTotal.schema.json", 'rb') as file:
         jsonschema = json.load(file)
-    conn = sqlite3.connect(db_name)
-    conn.execute("PRAGMA encoding = 'UTF-8';")
-    conn.commit()
+    columns = []
+    conn = None
+    if create:
+        conn = sqlite3.connect(db_name)
+        conn.execute("PRAGMA encoding = 'UTF-8';")
+        conn.commit()
     for (table_name, table_content) in jsonschema['properties'].items():
         assert (table_content['type'] == 'array')
         SQL = f"CREATE TABLE {table_name} (\n"
         for (att_name, att_content) in table_content['items']['properties'].items():
+            columns.append(att_name)
             SQL += f"  {att_name: <20} {'TEXT': <10},\n"
         SQL += "\n"
         SQL += "  PRIMARY KEY(id_lokalId, registreringFra, virkningFra)\n"
         SQL += ");\n"
-        conn.execute(SQL)
-    conn.commit()
-    conn.close()
+        prepare_table(table_name, columns)
+        if create:
+            conn.execute(SQL)
+    if create:
+        conn.commit()
+        conn.close()
+
 
 
 def main(data_package: 'file path to the zip datapackage',
@@ -87,9 +96,10 @@ def main(data_package: 'file path to the zip datapackage',
                 print("Deleting DB")
                 os.remove(db_name)
         print("Creating DB")
-        initialise_dar(db_name)
+    initialise_dar(db_name, create)
 
     conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
     with ZipFile(data_package, 'r') as myzip:
         #for info in myzip.infolist():
         #    print(info.filename)
@@ -119,16 +129,16 @@ def main(data_package: 'file path to the zip datapackage',
                 if '.' in prefix and event == 'start_map':
                     db_row = {}
                 if '.' in prefix and event == 'end_map':
-                    ret = insert_row(conn, db_table_name, db_row)
+                    ret = insert_row(cursor, db_table_name, db_row)
                     db_row = None
                     if ret == 0:
                         row_inserts += 1
                     else:
                         row_updates += 1
-                    if (row_inserts + row_updates) % step_rows == 0:
+                    if (row_inserts + row_updates) % STEP_ROWS == 0:
                         prev_step_time = step_time
                         step_time = time.time()
-                        print(f"{(row_inserts + row_updates):>10} rows inserted/updated in {db_table_name}. {step_rows//(step_time - prev_step_time)} rows/sec")
+                        print(f"{(row_inserts + row_updates):>10} rows inserted/updated in {db_table_name}. {int(STEP_ROWS // (step_time - prev_step_time))} rows/sec")
                 if event in ['null', 'boolean', 'integer', 'double', 'number',  'string']:
                     db_row[db_column] = value
                     db_column = None
