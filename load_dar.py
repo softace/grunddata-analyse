e1116580aa94bb4207b66c12981b6aa43d1f55a5
Jarl Friis <jarl@softace.dk>
2020-03-13 18:21:06 +0100

    introducing update and some validation

--- load_dar.py
+++ load_dar.py
@@ -11,16 +11,46 @@ import time
 step_rows = 1000000
 
 
+
 def insert_row(connection, listName, row):
     columns = row.keys()
     values = ['NULL' if (x is None) else x for x in row.values()]
-    SQL = f" INSERT into {listName} ({', '.join(columns)}) VALUES({', '.join(['?' for x in range(len(values))])});"
-    try:
+    if row['id_lokalId'] is None or row['registreringFra'] is None or row['virkningFra'] is None:
+        raise ValueError(f"Forventet primær nøgle (id_lokalId, registreringFra, virkningFra) har egentlig værdier, men fandt ({row['id_lokalId']}, {row['registreringFra']}, {row['virkningFra']})")
+    if row['virkningTil']: #This is an update
+        cursor = connection.cursor()
+        original = "select id_lokalId, registreringFra, virkningFra " \
+                   f"from {listName} where true " \
+                   "AND id_lokalId = ? " \
+                   "AND registreringFra = ? " \
+                   "AND virkningFra = ?"
+        cursor.execute(original, [row['id_lokalId'], row['registreringFra'], row['virkningFra']])
+        rows = cursor.fetchall()
+        if len(rows) > 1:
+            raise ValueError(f"Forventet at opdatere een forekomst for ({row['id_lokalId']}, {row['registreringFra']}, {row['virkningFra']}), fandt {len(rows)} forekomster")
+        else:
+            update = f"update {listName} set " + \
+                     ", ".join([f" {c} = ? " for c in columns]) + " where true "\
+                     "AND id_lokalId = ? " \
+                     "AND registreringFra = ? " \
+                     "AND virkningFra = ?"
+            cursor = connection.cursor()
+            cursor.execute(update, values + [row['id_lokalId'], row['registreringFra'], row['virkningFra']])
+            return 1
+    else:
+        select = f"select * from {listName} where true " \
+                 "AND registreringFra >= ? " \
+                 "AND (? < registreringTil or registreringTil is NULL) " \
+                 "AND virkningFra >= ? " \
+                 "AND ? < virkningTil or virkningTil is NULL"
+        cursor = connection.cursor()
+        cursor.execute(select, [row['registreringFra'], row['registreringTil'], row['virkningFra'], row['virkningTil']])
+        violations = cursor.fetchall()
+        if len(violations) > 0:
+            raise ValueError(f"Der findes allerede en forekomst i registreringstid ({row['registreringFra']}; {row['registreringTil']}) og virkningstid({row['virkningFra']}, {row['virkningTil']})")
+        SQL = f" INSERT into {listName} ({', '.join(columns)}) VALUES({', '.join(['?' for x in range(len(values))])});"
         connection.execute(SQL, values)
-    except Exception as e:
-        print(SQL)
-        raise e
-    return
+        return 0
 
 
 def initialise_dar(db_name):
@@ -69,7 +99,8 @@ def main(data_package: 'file path to the zip datapackage',
             db_table_name = None
             db_row = None
             db_column = None
-            rows = 0
+            row_inserts = 0
+            row_updates = 0
             start_time = time.time()
             step_time = time.time()
             for prefix, event, value in parser:
@@ -80,20 +111,25 @@ def main(data_package: 'file path to the zip datapackage',
                         db_table_name = value
                         print(f"Inserting into {db_table_name}")
                 if event == 'end_array':
-                    print(f"{rows} rows inserted into {db_table_name}")
-                    rows = 0
+                    print(f"{row_inserts} rows inserted into {db_table_name}")
+                    print(f"{row_updates} rows updated  in   {db_table_name}")
+                    row_inserts = 0
+                    row_updates = 0
                     db_table_name = None
                 if '.' in prefix and event == 'start_map':
                     db_row = {}
                 if '.' in prefix and event == 'end_map':
-                    insert_row(conn, db_table_name, db_row)
+                    ret = insert_row(conn, db_table_name, db_row)
                     db_row = None
-                    rows += 1
-                    if rows % step_rows == 0:
+                    if ret == 0:
+                        row_inserts += 1
+                    else:
+                        row_updates += 1
+                    if (row_inserts + row_updates) % step_rows == 0:
                         prev_step_time = step_time
                         step_time = time.time()
-                        print(f"{rows} rows inserted into {db_table_name}. {step_rows//(step_time - prev_step_time)} rows/sec")
-                if event == 'string':
+                        print(f"{(row_inserts + row_updates):>10} rows inserted/updated in {db_table_name}. {step_rows//(step_time - prev_step_time)} rows/sec")
+                if event in ['null', 'boolean', 'integer', 'double', 'number',  'string']:
                     db_row[db_column] = value
                     db_column = None
     conn.commit()
