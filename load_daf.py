a30e3a42d9bbe74bf83c99eb4f83708b2caa4475
Jarl Friis <jarl@softace.dk>
2020-03-20 18:18:44 +0100

    Generalised to prepare for Postgresql

--- load_daf.py
+++ load_daf.py
@@ -11,7 +11,8 @@ import time
 from datetime import timezone
 import dateutil.parser
 
-
+SQLITE = 'sqlite'
+POSTGRESQL = 'pg'
 STEP_ROWS = 1000000
 table_names = {}
 
@@ -68,16 +69,18 @@ def insert_row(cursor, listName, row):
         raise e
 
 
-def prepare_table(table_name, columns):
+def prepare_table(table):
+    table_name = table['name']
+    column_names = [c['name'] for c in table['columns']]
     table_names[table_name] = {}
-    table_names[table_name]['row'] = dict(zip(columns,[None for i in range(len(columns))]))
+    table_names[table_name]['row'] = dict(zip(column_names, [None for i in range(len(column_names))]))
     table_names[table_name]['F'] = "select id_lokalId, registreringFra_UTC, virkningFra_UTC " \
                                    f"from {table_name} where true " \
                                    "AND id_lokalId = ? " \
                                    "AND registreringFra_UTC = ? " \
                                    "AND virkningFra_UTC = ?"
     table_names[table_name]['U'] = f"update {table_name} set " + \
-                                   ", ".join([f" {c} = ? " for c in columns]) + " where true "\
+                                   ", ".join([f" {c} = ? " for c in column_names]) + " where true " \
                                    "AND id_lokalId = ? " \
                                    "AND registreringFra_UTC = ? " \
                                    "AND virkningFra_UTC = ?"
@@ -87,90 +90,148 @@ def prepare_table(table_name, columns):
                                    "  OR _RegistreringFra_UTC <=  registreringFra_UTC AND ( registreringFra_UTC < _RegistreringTil_UTC OR _RegistreringTil_UTC is NULL)) "\
                                    "AND ( virkningFra_UTC <= _VirkningFra_UTC AND (_VirkningFra_UTC <  virkningTil_UTC OR  virkningTil_UTC is NULL) "\
                                    "  OR _VirkningFra_UTC <=  virkningFra_UTC AND ( virkningFra_UTC < _VirkningTil_UTC OR _VirkningTil_UTC is NULL)) "
-    table_names[table_name]['I'] = f" INSERT into {table_name} ({', '.join(columns)})"\
-                                   f" VALUES({', '.join(['?' for x in range(len(columns))])});"
+    table_names[table_name]['I'] = f" INSERT into {table_name} ({', '.join(column_names)})" \
+                                   f" VALUES({', '.join(['?' for x in range(len(column_names))])});"
 
 
-def initialise_db(db_name, jsonschema, create=False):
-    sqlite3.register_adapter(decimal.Decimal, decimal2text)
-    sqlite3.register_converter('NUMERIC', text2decimal)  # It is most efficient to use storage class NUMERIC
-
-    conn = None
-    if create:
-        # The detect_type is not relevant here in the creation phase, I believe.
-        conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES)
-        conn.execute("PRAGMA encoding = 'UTF-8';")
-        conn.commit()
-    for (table_name, table_content) in jsonschema['properties'].items():
-        assert (table_content['type'] == 'array')
-        columns = []
-        SQL = f"CREATE TABLE {table_name} (\n"
-        for (att_name, att_content) in table_content['items']['properties'].items():
-            type_spec = ''
-            if att_content['type'][0] == 'string':
-                if 'format' in att_content:
-                    if att_content['format'] == 'date-time':
-                        type_spec += ' TEXT'  # TODO: improve
-                    else:
-                        raise NotImplementedError(f"Unknown attribute format '{att_content['format']}' on attribute {att_name}.")
-                else:
-                    type_spec += ' TEXT'
-            elif att_content['type'][0] == 'integer':
-                type_spec += 'INT'
-            elif att_content['type'][0] == 'number':
-                type_spec += 'NUMERIC'  # This will trigger the converter
-            else:
-                raise NotImplementedError(f"Unknown attribute type '{att_content['type'][0]}' on attribute {att_name}.")
-            if att_content['type'][1] == 'null':
+def sqlite3_create_table(table):
+    SQL = f"CREATE TABLE {table['name']} (\n"
+    for (column) in table['columns']:
+        # table_content['items']['properties'].items()
+        type_spec = ''
+        TYPE_MAPPING = {
+            'string': 'TEXT',
+            'datetime': 'TEXT',  # TODO: This could be improved
+            'integer': 'INT',
+            'number': 'NUMERIC'  # This will ensure affinity and trigger the converter
+        }
+        if column['type'] not in TYPE_MAPPING:
+            raise NotImplementedError(f"Unknown columns type '{column['type']}' on column {repr(column)}.")
+        type_spec += TYPE_MAPPING[column['type']]
+        if 'nullspec' in column:
+            if column['nullspec'] == 'null':
                 type_spec += ' NULL'
             else:
-                raise NotImplementedError(f"Unknown attribute nullification '{att_content['type'][1]}' on attribute {att_name}.")
-            columns.append(att_name)
-            SQL += f"  {att_name: <20} {type_spec: <10}, -- {att_content['description']}\n"
-            if att_name in ['registreringFra', 'registreringTil', 'virkningFra', 'virkningTil']:
-                SQL += f"  {att_name + '_UTC': <20} {type_spec: <10},  -- TZ neutral\n"
-                columns.append(att_name + '_UTC')
-        SQL += "\n"
-        SQL += "  PRIMARY KEY(id_lokalId, registreringFra_UTC, virkningFra_UTC)\n"
-        SQL += ");\n"
-        prepare_table(table_name, columns)
-        if create:
-            conn.execute(SQL)
-    if create:
-        SQL = "CREATE TABLE violation_log ("\
-              "  number INTEGER PRIMARY KEY AUTOINCREMENT, "\
-              "  table_name TEXT, "\
-              "  id_lokalId TEXT, "\
-              "  conflicting_registreringFra_UTC TEXT, "\
-              "  conflicting_virkningFra_UTC TEXT, "\
-              "  violating_registreringFra_UTC TEXT, "\
-              "  violating_virkningFra_UTC TEXT"\
-              ")"
-        conn.execute(SQL)
+                raise NotImplementedError(
+                    f"Unknown column nullification '{column['nullspec']}' on column {repr(column)}.")
+        comment = f"  --  {column['description']}" if 'description' in column else ''
+        SQL += f"  {column['name']: <20} {type_spec: <10},{comment}\n"
+    SQL += "\n"
+    SQL += "  PRIMARY KEY(" + ','.join(table['primary_keys']) + ")\n"
+    SQL += ");\n"
+    return SQL
+
+
+def jsonschema2table(table_name, table_content):
+    table = {'name': table_name,
+             'columns': [],
+             'primary_keys': []
+             }
+    for (att_name, att_content) in table_content['items']['properties'].items():
+        if att_content['type'][0] == 'string':
+            if 'format' in att_content:
+                if att_content['format'] == 'date-time':
+                    type = 'datetime'
+                else:
+                    raise NotImplementedError(
+                        f"Unknown attribute format '{att_content['format']}' on attribute {att_name}.")
+            else:
+                type = 'string'
+        elif att_content['type'][0] == 'integer':
+            type = 'integer'
+        elif att_content['type'][0] == 'number':
+            type = 'number'  # This will trigger the converter
+        else:
+            raise NotImplementedError(f"Unknown attribute type '{att_content['type'][0]}' on attribute {att_name}.")
+        if att_content['type'][1] == 'null':
+            nullspec = 'null'
+        else:
+            raise NotImplementedError(
+                f"Unknown attribute nullification '{att_content['type'][1]}' on attribute {att_name}.")
+        column = {'name': att_name,
+                  'type': type,
+                  'nullspec': nullspec,
+                  'description': att_content['description'] if 'description' in att_content else None
+                  }
+        table['columns'].append(column)
+        if att_name in ['registreringFra', 'registreringTil', 'virkningFra', 'virkningTil']:
+            table['columns'].append({'name': att_name + '_UTC',
+                                     'type': 'datetime',
+                                     'nullspec': 'null',
+                                     'description': 'TZ neutral'
+                                     })
+    table['primary_keys'] = ['id_lokalId', 'registreringFra_UTC', 'virkningFra_UTC']
+    return table
+
+
+def initialise_db(dbo, create, force, jsonschema):
+    if dbo['backend'] == SQLITE:
+        sqlite3.register_adapter(decimal.Decimal, decimal2text)
+        sqlite3.register_converter('NUMERIC', text2decimal)  # It is most efficient to use storage class NUMERIC
+        conn = sqlite3.connect(dbo['name'] + '.db', detect_types=sqlite3.PARSE_DECLTYPES)
+        conn.execute("PRAGMA encoding = 'UTF-8';")
         conn.commit()
-        conn.close()
+    elif dbo['backend'] == POSTGRESQL:
+        conn = None
+    else:
+        raise NotImplementedError(
+            f"Unknown database backend '{dbo['backend']}'.")
+
+    cur = conn.cursor()
+    for (table_name, table_content) in jsonschema['properties'].items():
+        assert (table_content['type'] == 'array')
+        table = jsonschema2table(table_name, table_content)
+        prepare_table(table)
+        if create:
+            if force:
+                cur.execute(f"DROP TABLE IF EXISTS {table['name']}")
+            cur.execute(sqlite3_create_table(table))
+    table = {
+        'name': 'violation_log',
+        'columns': [{'name': 'number', 'type': 'integer'},
+                    {'name': 'table_name', 'type': 'string'},
+                    {'name': 'id_lokalId', 'type': 'string'},
+                    {'name': 'conflicting_registreringFra_UTC', 'type': 'string'},
+                    {'name': 'conflicting_virkningFra_UTC', 'type': 'string'},
+                    {'name': 'violating_registreringFra_UTC', 'type': 'string'},
+                    {'name': 'violating_virkningFra_UTC', 'type': 'string'},
+                    ],
+        'primary_keys': ['number'],
+    }
+    #  Consider prepare_table(table)
+    if create:
+        if force:
+            cur.execute(f"DROP TABLE IF EXISTS {table['name']}")
+        cur.execute(sqlite3_create_table(table))
+    conn.commit()
+    return conn
 
 
-def main(create: ("Create the database", 'flag', 'c'),
-         force: ("Force the DB creation", 'flag', 'f'),
+def main(create: ("Create the tables before inserting", 'flag', 'C'),
+         force: ("Force creation of tables (DROP) if they already exists", 'flag', 'F'),
+         db_backend: ("DB backend. Supported is 'sqlite', 'pg", 'option', 'b'),
+         db_host: ("Database host", 'option', 'H'),
+         db_port: ("Database port", 'option', 'p'),
+         db_name: ('Database name, defaults to <registry>', 'option', 'd'),
+         db_user: ("Database user", 'option', 'u'),
+         db_password: ("Database password", 'option', 'X'),
          registry: ("DAF register: dar, bbr", 'option', 'r'),
-         data_package: 'file path to the zip datapackage',
-         db_name: 'Database file, defaults to <registry>.db' = None):
+         data_package: 'file path to the zip datapackage'):
     """Loads a DAF data file into database"""
 
-    if not db_name:
-        db_name = registry + ".db"
+    database_options = {
+        'backend': db_backend,
+        'host': db_host,
+        'port': db_port,
+        'name': db_name if db_name else registry,
+        'user': db_user,
+        'password': db_password
+    }
+
     if not data_package[-4:] == '.zip':
         raise ValueError("data_package must be a zip file and end with '.zip'")
     package_name = data_package[:-4]
     print(f'Loading data from {package_name}')
-    if create:
-        if force:
-            with contextlib.suppress(FileNotFoundError):
-                print("Deleting DB")
-                os.remove(db_name)
-        print("Creating DB")
-
     if registry == 'dar':
         json_schema_file_name = "DAR_v2.3.6_2019.08.18_DLS/DAR_v2.3.6_2019.08.19_DARTotal.schema.json"
     elif registry == 'bbr':
@@ -178,12 +239,11 @@ def main(create: ("Create the database", 'flag', 'c'),
     else:
         raise ValueError(f"Ukendt register '{registry}'.")
     with open(json_schema_file_name, 'rb') as file:
-        initialise_db(db_name, json.load(file), create)
+        conn = initialise_db(database_options, create, force, json.load(file))
 
-    conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES)
     cursor = conn.cursor()
     with ZipFile(data_package, 'r') as myzip:
-        #for info in myzip.infolist():
+        # for info in myzip.infolist():
         #    print(info.filename)
         json_data_name = next(x for x in myzip.namelist() if not 'Metadata' in x)
         with myzip.open(json_data_name) as file:
@@ -226,7 +286,7 @@ def main(create: ("Create the database", 'flag', 'c'),
                         prev_step_time = step_time
                         step_time = time.time()
                         print(f"{(row_inserts + row_updates):>10} rows inserted/updated in {db_table_name}. {int(STEP_ROWS // (step_time - prev_step_time))} rows/sec")
-                if event in ['null', 'boolean', 'integer', 'double', 'number',  'string']:
+                if event in ['null', 'boolean', 'integer', 'double', 'number', 'string']:
                     db_row[db_column] = value
                     db_column = None
     conn.commit()
